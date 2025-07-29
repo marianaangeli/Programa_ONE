f5158b212b152ab86a6ac4d9c8b7198b33c9c34b
Mariana <marianaangeliyoga@gmail.com>
2025-07-29 14:18:49 -0300

    Add files via upload

---
+++ Logica-de-Programacao/listas-js/listas.js
@@ -0,0 +1,23 @@
+// Cria uma lista vazia chamada listaGenerica
+let listaGenerica = [];
+
+// Cria uma lista de linguagens de programação com os elementos indicados
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// Adiciona os elementos 'Java', 'Ruby' e 'GoLang' à lista linguagensDeProgramacao
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+// Cria uma lista com 3 nomes
+let nomes1 = ['Ana', 'Bruno', 'Carlos'];
+// Exibe no console apenas o primeiro elemento
+console.log(nomes1[0]); // Ana
+
+// Cria uma lista com 3 nomes
+let nomes2 = ['Daniela', 'Eduardo', 'Fernanda'];
+// Exibe no console apenas o segundo elemento
+console.log(nomes2[1]); // Eduardo
+
+// Cria uma lista com 3 nomes
+let nomes3 = ['Gabriela', 'Hugo', 'Isabela'];
+// Exibe no console apenas o último elemento
+console.log(nomes3[nomes3.length - 1]); // Isabela
